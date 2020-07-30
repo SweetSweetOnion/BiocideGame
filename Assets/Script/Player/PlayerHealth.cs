b933cabfebecf13e416822f58bb92e8542bf031e
Nicolas <n.leray94@gmail.com>
2020-07-30 10:38:29 +0200

    [Add] player damage cd

--- Assets/Script/Player/PlayerHealth.cs
+++ Assets/Script/Player/PlayerHealth.cs
@@ -5,8 +5,10 @@ using System.Runtime.InteropServices;
 public class PlayerHealth : MonoBehaviour
 {
 	public int startHp = 3;
+	public float damageCooldown = 1;
 
 	private int _currentHp;
+	private float _lastDamage = -1;
 
 	public delegate void BasicEvent();
 	public event BasicEvent OnDead;
@@ -15,7 +17,9 @@ public class PlayerHealth : MonoBehaviour
 	public int currentHp => _currentHp;
 
 	public void ReceivedDamage(int amount, Vector3 worldOrigin){
+		if (Time.time <= _lastDamage + damageCooldown) return;
 		_currentHp -= amount;
+		_lastDamage = Time.time;
 		OnDamage?.Invoke(amount, worldOrigin);
 		if(_currentHp <= 0){
 			OnDead?.Invoke();
