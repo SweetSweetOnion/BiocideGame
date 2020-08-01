c264f0629d4410d318825517a9669027e49e0e65
Nicolas <n.leray94@gmail.com>
2020-08-01 13:47:40 +0200

    [ADD] New LD

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -209,8 +209,8 @@ public class PlayerController : MonoBehaviour
 	private void UpdateCurrentTiles(){
 		Vector3 bottomLeftPos = transform.position - Vector3.up * _boxCollider.size.y / 2 + Vector3.left * _boxCollider.size.x /2;
 		Vector3 bottomRightPos = transform.position - Vector3.up * _boxCollider.size.y / 2 - Vector3.left * _boxCollider.size.x /2;
-		currentTiles[0] = TileManager.mainTilemap.WorldToCell(bottomLeftPos - Vector3.up *0.1f);
-		currentTiles[1] = TileManager.mainTilemap.WorldToCell(bottomRightPos - Vector3.up * 0.1f);
+		currentTiles[0] = TileManager.mainTilemap.WorldToCell(bottomLeftPos - Vector3.up *0.1f + Vector3.left *0.1f );
+		currentTiles[1] = TileManager.mainTilemap.WorldToCell(bottomRightPos - Vector3.up * 0.1f - Vector3.left * 0.1f);
 		if(isGrounded){
 			TileManager.OnTilesWalkedOn(currentTiles, this);
 		}
