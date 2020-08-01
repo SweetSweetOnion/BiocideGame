1e53f28cecccff762a1a0438ff008403fb6d3a9a
Valentin <valentin-didomenico@orange.fr>
2020-08-01 17:06:50 +0200

    [ADD] Death Sound

--- Assets/Script/Player/PlayerHealth.cs
+++ Assets/Script/Player/PlayerHealth.cs
@@ -30,8 +30,8 @@ public class PlayerHealth : MonoBehaviour
 		OnDamage?.Invoke(amount, worldOrigin);
 		if(_currentHp <= 0){
 			_isDead = true;
-			OnDead?.Invoke();
             AudioManager.instance.FOLEYS_Char_Death.Post(gameObject);
+            OnDead?.Invoke();
 		}
         //SOUND
         AudioManager.instance.FOLEYS_Char_Hit.Post(gameObject);
