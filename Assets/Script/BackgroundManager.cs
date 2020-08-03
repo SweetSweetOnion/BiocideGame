be66d9a3b80eca1adb0963856a01759a6d9a8280
Nicolas <n.leray94@gmail.com>
2020-08-03 18:25:28 +0200

    [Add] sun and moon

--- Assets/Script/BackgroundManager.cs
+++ Assets/Script/BackgroundManager.cs
@@ -32,10 +32,34 @@ public class BackgroundManager : MonoBehaviour
 	public Color matEnd;
 
 	public float offset = 0;
+	public Transform sunMoonRotation;
+
+	private float currentValue = 0;
+	private float sunMoonPos;
+
+	private void Start()
+	{
+		sunMoonPos = controller.transform.position.x;
+	}
+
 
 	private void Update()
 	{
-		float x =  Mathf.Abs(Mathf.Sin(Mathf.Clamp01(Mathf.InverseLerp(minX, maxX, controller.transform.position.x )) * Mathf.PI + offset));
+		float n = Mathf.Clamp01(Mathf.InverseLerp(minX, maxX, controller.transform.position.x));
+		if (currentValue - n >0.3f )
+		{
+			currentValue = 0;
+			sunMoonPos = controller.transform.position.x;
+		}
+		currentValue = Mathf.MoveTowards(currentValue, Mathf.Max(currentValue, n), Time.deltaTime * 5);
+
+
+		sunMoonPos = Mathf.MoveTowards(sunMoonPos, Mathf.Max(sunMoonPos, controller.transform.position.x), Time.deltaTime * 15);
+
+		float x =  Mathf.Abs(Mathf.Sin(currentValue * Mathf.PI + offset));
+
+		sunMoonRotation.rotation = Quaternion.Euler(0, 0, -currentValue * 360);
+		sunMoonRotation.transform.position = new Vector3(sunMoonPos, sunMoonRotation.position.y, sunMoonRotation.position.z);
 
 		for(int i = 0; i< background.Length; i++){
 			background[i].color = Color.Lerp(backColorStart[i], backColorEnd[i], x);
@@ -66,6 +90,8 @@ public class BackgroundManager : MonoBehaviour
 
 		}
 		materialColor.color = Color.Lerp(matStart, matEnd, f);
+		sunMoonRotation.rotation = Quaternion.Euler(0, 0, -f * 180);
+
 
 	}
 }
